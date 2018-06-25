4f527dc760d7171978f6dda4b30e60a22fdd132f
Rüdiger Schulz <rueschu@gmail.com>
2018-06-25 11:35:48 +0200

    start.spring.io generated empty project

---
+++ src/main/java/com/mindhaq/micrometerbasics/MicrometerbasicsApplication.java
@@ -0,0 +1,12 @@
+package com.mindhaq.micrometerbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerbasicsApplication.class, args);
+	}
+}
